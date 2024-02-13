f7644055725cd725a69d9a831401a7dd1b04fccb
Paulo19961944 <paulo24041996@gmail.com>
2024-02-13 14:22:11 -0300

    Jornada do Dev | Front-End

---
+++ JS/01-script.js
@@ -0,0 +1,29 @@
+// DECLARANDO UMA VARIÁVEL
+let nomeDaVariavel = 5;
+let fruta = "Maçã";
+
+// TIPOS DE DADOS
+
+// Strings
+let nome = "Paulo";
+
+// Inteiros
+let idade = 27;
+
+// Float
+let nota = 9.5;
+
+// Booleano
+let sexoMasculino = true;
+
+// Arrays
+let frutas = ["Banana", "Maçã", "Uva"];
+
+// Acessa o primeiro item da Array
+frutas[0];
+
+// Objetos
+let carro = {marca:"Chevrolet", modelo:"Blazer", cor:"Prata", peso: 1740};
+
+// Acessa a propriedade do Objeto
+carro.modelo;
---
+++ JS/02-string.js
@@ -0,0 +1,9 @@
+let string = "Paulo";
+let hello = "Olá";
+let welcome = "Seja bem Vindo!!!"
+let pessoa = "Paulo";
+let msg = `Olá ${pessoa}, seja bem vindo`;
+
+// Exercicio
+let nameUser = prompt("Digite o seu nome: ")
+alert(`Olá ${nameUser}, seja bem vindo`);
